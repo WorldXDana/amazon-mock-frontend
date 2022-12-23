6c9217d72ca6730f1300396aaaaedb1e98fcde04
WorldXDana <depshtein1808@gmail.com>
2022-12-24 00:03:59 +0200

    push directory.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './Componets/NavBar/Navigation';
+import MainPage from './Componets/HomePage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+        <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/NavBar/Navigation.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import "./NavBar.css"
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+    render() {
+        return ( 
+            <div className='navbar_component'>
+                Navbar
+            </div>
+        );
+    }
+}
+
+export default NavBar;
